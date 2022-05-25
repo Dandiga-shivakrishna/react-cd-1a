3d9b0e4cdd527a5974276e2d8629fd16d8eba872
Shiva krishna <shivakrishna585@gmail.com>
2022-05-25 09:36:58 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="back">
+    <div className="content-center">
+      <h1 class="heading">Super over League</h1>
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+          className="img"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+          className="img"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
